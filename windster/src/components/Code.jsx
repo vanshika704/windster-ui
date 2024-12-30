5f3f1274223edc71fcf0bba82d74a3a9f3965da2
jasika03 <jasika1452@gmail.com>
2024-12-30 18:54:25 +0530

    main-left

--- windster/src/components/Code.jsx
+++ windster/src/components/Code.jsx
@@ -1,112 +1,99 @@
 import React from "react";
 
+
 function Code() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img
-              src="http://127.0.0.1:5500/windster-ui/windster/src/assets/windsteruilogo.svg"
-              alt="Logo"
-              width="30"
-              height="24"
-              class="d-inline-block align-text-top"
-            />
-            <strong> Windster</strong>
+        <div
+          className="sidebar"
+          style={{ border: "1.5px solid #EEEEEE", borderTop: "none" }}
+        >
+          <a href="#">
+            <i class="fa-solid fa-chart-pie"></i>Dashboard
           </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+          <a href="#">
+            <i class="fa-solid fa-boxes-stacked"></i>Kanban <button style={{padding:"0vh 1vh 0vh 1vh", border:"none", borderRadius: "10px", marginLeft:"10vh"}}>Pro</button>
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-inbox"></i>Inbox <button style={{padding:"0vh 1vh 0vh 1vh", border:"none", borderRadius: "10px", marginLeft:"12.2vh"}}>Pro</button>
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-user"></i>User
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-bag-shopping"></i>Products
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>Sign In
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-table"></i>Sign Up
+          </a>
+          <hr style={{ color: "gray" }} />
+          <a href="#">
+            <i class="fa-solid fa-gem"></i>Upgrade To Pro
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-clipboard-list"></i>Documentation
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-layer-group"></i>Components
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-circle-radiation"></i>Help
+          </a>
+        </div>
+        <div
+          className="topnav"
+          style={{ border: "1.5px solid #EEEEEE", borderTop: "none" }}
+        >
+          <a class="active" href="#logo">
+            <img src="windster-ui\windster\src\assets\windsteruilogo.svg"/>
+          </a>
+          <a href="#">
+            <strong>Windster</strong>
+          </a>
+          <div
+            className="search"
+            style={{
+              marginLeft: "17vh",
+              marginBottom:"1vh",
+              border: "1px solid #dee2e6 ",
+              borderRadius: "10px",
+              display: "flex",
+              backgroundColor: "#f6f9fc",
+              alignItems: "center",
+              padding: "1vh",
+              paddingTop: "0",
+              width: "35vh",
+            }}
           >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <form class="d-flex e-5" role="search">
+            <i
+              class="fa-solid fa-magnifying-glass"
+              style={{
+                marginRight: "10px",
+                marginTop: "8px",
+                color: "#9E9E9E",
+              }}
+            ></i>
             <input
-              class="form-control me-2"
-              type="search"
+              type="text"
               placeholder="Search"
-              aria-label="Search"
+              style={{
+                border: "none",
+                background: "transparent",
+                marginTop: "8px",
+              }}
             />
-          </form>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-            </ul>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-              <button class="btn btn-secondary btn-sm me-md-2" type="submit">
-                <i class="bi bi-star"> Star</i>
-              </button>
-              <button class="btn btn-primary" type="button">
-                <i class="bi bi-gem"> Upgrade to Pro</i>
-              </button>
-            </div>
           </div>
+          <a style={{marginLeft:"78vh", color:"#808080"}}>Open source <i class="fa-solid fa-heart" style={{color: "red"}}></i></a>
+          <button style={{padding:"0vh 1.3vh 0vh 1.3vh", border:"1px solid #dee2e6", borderRadius: "3px", marginBottom:"10px"}}><i class="fa-regular fa-star"></i> Star</button>
+          <button style={{marginLeft:"2.5vh", marginBottom:"5px", border:"none", borderRadius:"10px", background:"#00ACC1", padding:"1vh 2vh 1vh 2vh", color:"white"}}><i class="fa-solid fa-gem"></i> Upgrade to Pro</button>
         </div>
-      </nav>
-      <div class="row">
-        <div class="col-2">
-          <nav
-            id="navbar-example3"
-            class="h-100 flex-column align-items-stretch pe-4 border-end">
-            <nav class="nav nav-pills flex-column">
-              <a class="nav-link" href="#">
-                <i class="bi bi-pie-chart-fill"> Dashboard</i>
-              </a>
-              <a class="nav-link " href="#">
-                <i class="bi bi-grid-fill">Kanban</i>
-              </a>
-              <a class="nav-link " href="#">
-                <i class="bi bi-inbox-fill"> Inbox</i>
-              </a>
-              <a class="nav-link" href="#2">
-                <i class="bi bi-person-circle"> Users</i>
-              </a>
-              <a class="nav-link" href="#">
-                <i class="bi bi-handbag-fill"> Products</i>
-              </a>
-              <a class="nav-link " href="#">
-                <i class="bi bi-box-arrow-in-right"> Sign-in</i>
-              </a>
-              <a class="nav-link " href="#">
-                <i class="bi bi-postcard-fill"> Sign-up</i>
-              </a>
-              <hr />
-              <a class="nav-link" href="#">
-                <i class="bi bi-gem"> Upgrade to Pro</i>
-              </a>
-              <a class="nav-link " href="#">
-                <i class="bi bi-file-earmark-text-fill"> Documentation</i>
-              </a>
-              <a class="nav-link " href="#">
-                <i class="bi bi-collection-fill"> Components</i>
-              </a>
-              <a class="nav-link" href="#2">
-                <i class="bi bi-question-circle-fill"> Help</i>
-              </a>
-            </nav>
-          </nav>
+        <div className="main" style={{ backgroundColor: "#f6f9fc", height: "calc(100vh - 7.3vh)", marginLeft: "16.6vw", width: "calc(100% - 16.6vw)"}}>
         </div>
-      </div>
     </>
   );
 }
 
 export default Code;
-{
-  /* 
-    <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-    </form>
-    </nav>
-    </div> */
-}
